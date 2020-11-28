af1bbc9626c0584d65f085595e053f212190b7bc
khmo529 <khmo529@naver.com>
2020-11-28 18:37:16 +0900

    big_image

--- main.py
+++ main.py
@@ -5,19 +5,21 @@ import urllib.request
 
 a=input("검색할 키워드를 입력하세요 : ")
 driver = webdriver.Chrome()
-driver.get('https://www.google.co.kr/imghp?hl=ko')
+driver.get('http://www.google.co.kr/imghp?hl=ko')
 elem = driver.find_element_by_name("q")
 elem.send_keys(a)
 elem.send_keys(Keys.RETURN)
 images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
-
 count = 0
 for image in images:
-    image.click()
-    time.sleep(2)
-    imgUrl = driver.find_element_by_css_selector(".n3VNCb").get_attribute("src")
-    urllib.request.urlretrieve(imgUrl, "imgfile/" + a + str(count) + ".jpg")
-    count += 1
-    if count == 5:
-        break
-#browser.quit()
+    try:
+        image.click()
+        imgUrl = driver.find_element_by_xpath("/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div[1]/div[1]/div/div[2]/a/img").get_attribute("src")
+        urllib.request.urlretrieve(imgUrl, "imgfile/" + a + str(count) + ".jpg")
+        count += 1
+        if count == 10:
+            break
+    except:
+        pass
+            
+driver.close()
