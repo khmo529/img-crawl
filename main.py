fb223c571d253a78a9fb3c2ea3bb2c097baaf25b
khmo529 <khmo529@naver.com>
2020-11-28 18:48:11 +0900

    20201128\

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 import urllib.request
 
 a=input("검색할 키워드를 입력하세요 : ")
+b=int(input("개수 : "))
 driver = webdriver.Chrome()
 driver.get('http://www.google.co.kr/imghp?hl=ko')
 elem = driver.find_element_by_name("q")
@@ -17,9 +18,8 @@ for image in images:
         imgUrl = driver.find_element_by_xpath("/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div[1]/div[1]/div/div[2]/a/img").get_attribute("src")
         urllib.request.urlretrieve(imgUrl, "imgfile/" + a + str(count) + ".jpg")
         count += 1
-        if count == 10:
+        if count == b:
             break
     except:
         pass
-            
 driver.close()
